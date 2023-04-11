da448dcd6947183862ff74f2183b8ab7ec62e4d7
JESurzyn <jordan.surzyn@gmail.com>
2023-04-11 07:58:14 -0400

    finished react router demo

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Root, { loader as rootLoader,
 import ErrorPage from './error-page'
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from './routes/contact';
 import EditContact, {action as editAction} from './routes/edit';
 import { action as destroyAction } from './routes/destroy';
@@ -25,25 +26,31 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        index: true,
-        element: <Index />
+        errorElement: <ErrorPage />,
+        children: [
+              {    
+                index: true,
+                element: <Index />
+              },
+              {
+                path: 'contacts/:contactId',
+                element: <Contact />,
+                loader: contactLoader,
+                action: contactAction,
+              },
+              {
+                path: 'contacts/:contactId/edit',
+                element: <EditContact />,
+                loader: contactLoader,
+                action: editAction,
+              },
+              {
+                path: 'contacts/:contactId/destroy',
+                action: destroyAction,
+                errorElement: <div>Oops! There was an error.</div>
+              }
+            ],
       },
-      {
-        path: 'contacts/:contactId',
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: 'contacts/:contactId/edit',
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: 'contacts/:contactId/destroy',
-        action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>
-      }
     ],
   },
 ]);
